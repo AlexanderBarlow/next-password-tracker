efb9c2fae293b210aac598c416946a9ead7f2e91
Alexander Barlow <alex.barlow@rocketmail.com>
2023-12-28 15:20:00 -0500

    deployment fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
+// next.config.js
+// Importing environment variables if needed
 await import("./src/env.js");
 
-/** @type {import("next").NextConfig} */
-const config = {};
+// Configuration object
+const config = {
+  // Your other configuration options go here
 
+  // Remove the "target" property
+
+  // If you were using "target: 'serverless'", you can use the "target: 'experimental-serverless-trace'" option instead:
+  experimental: {
+    serverlessTracing: true,
+  },
+
+  // If you were using "target: 'server'", the default behavior is now serverless, so you can simply remove the "target" property.
+};
+
+// Export the configuration
 export default config;
